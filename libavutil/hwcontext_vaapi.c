64277b7da8bf7cdf56ee48040ba576f2f3eb76f6
Haihao Xiang <haihao.xiang@intel.com>
2021-01-04 10:46:14 +0800

    qsv: use a new method to create mfx session when using oneVPL
    In oneVPL, MFXLoad() and MFXCreateSession() are required to create a
    workable mfx session[1]
    
    Add AccelerationMode config filter for D3D9/D3D11 session (galinart)
    
    The default device is changed to d3d11va for oneVPL when both d3d11va
    and dxva2 are enabled on Microsoft Windows
    
    This is in preparation for oneVPL support
    
    [1] https://spec.oneapi.io/versions/latest/elements/oneVPL/source/programming_guide/VPL_prg_session.html#onevpl-dispatcher
    
    Signed-off-by: galinart <artem.galin@intel.com>

--- libavutil/hwcontext_vaapi.c
+++ libavutil/hwcontext_vaapi.c
@@ -1530,6 +1530,7 @@ static void vaapi_device_free(AVHWDeviceContext *ctx)
     if (priv->drm_fd >= 0)
         close(priv->drm_fd);
 
+    av_free(hwctx->device_name);
     av_freep(&priv);
 }
 
@@ -1578,6 +1579,7 @@ static int vaapi_device_connect(AVHWDeviceContext *ctx,
 static int vaapi_device_create(AVHWDeviceContext *ctx, const char *device,
                                AVDictionary *opts, int flags)
 {
+    AVVAAPIDeviceContext *hwctx = ctx->hwctx;
     VAAPIDevicePriv *priv;
     VADisplay display = NULL;
     const AVDictionaryEntry *ent;
@@ -1623,6 +1625,11 @@ static int vaapi_device_create(AVHWDeviceContext *ctx, const char *device,
                        "DRM device node.\n", device);
                 break;
             }
+
+            hwctx->device_name = av_strdup(device);
+
+            if (!hwctx->device_name)
+                return AVERROR(ENOMEM);
         } else {
             char path[64];
             int n, max_devices = 8;
@@ -1663,6 +1670,12 @@ static int vaapi_device_create(AVHWDeviceContext *ctx, const char *device,
                     av_log(ctx, AV_LOG_VERBOSE, "Trying to use "
                            "DRM render node for device %d.\n", n);
                 }
+
+                hwctx->device_name = av_strdup(path);
+
+                if (!hwctx->device_name)
+                    return AVERROR(ENOMEM);
+
                 break;
             }
             if (n >= max_devices)
