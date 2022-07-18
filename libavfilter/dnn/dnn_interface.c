8de07e11f36456aa69ad08d374719357c00ad7e8
Ting Fu <ting.fu@intel.com>
2022-07-18 10:13:04 +0800

    lavfi: add LibTorch backend multi-frame inference support
    Dnn models may take multiple frames as one input, so add "nb_input"
    option to control input frame number. Use queue to buffer the input
    frames and send to model at once.
    
    Signed-off-by: Ting Fu <ting.fu@intel.com>
    Signed-off-by: Wenbin Chen <wenbin.chen@intel.com>

--- libavfilter/dnn/dnn_interface.c
+++ libavfilter/dnn/dnn_interface.c
@@ -40,6 +40,8 @@ static const AVOption dnn_base_options[] = {
                 OFFSET(model_filename), AV_OPT_TYPE_STRING, {.str = NULL}, 0, 0, FLAGS},
         {"input", "input name of the model",
                 OFFSET(model_inputname), AV_OPT_TYPE_STRING, {.str = NULL}, 0, 0, FLAGS},
+        { "input_nb", "input number of the mode",
+                OFFSET(nb_inputs), AV_OPT_TYPE_INT, { .i64 = 1 }, 0, INT_MAX, FLAGS },
         {"output", "output name of the model",
                 OFFSET(model_outputnames_string), AV_OPT_TYPE_STRING, {.str = NULL}, 0, 0, FLAGS},
         {"backend_configs", "backend configs (deprecated)",
